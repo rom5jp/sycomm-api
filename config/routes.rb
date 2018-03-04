ebbd17cfa53584e133e571b5f0e110d5ab49ece4
Romero Medeiros <romeromedeiros5@gmail.com>
2018-03-04 01:01:47 -0300

    Configuração inicial de versionamento de API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: false }, path: '/' do
+    namespace :v1, constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      resources :masters
+      resources :employess
+      resources :customers
+    end
+  end
 end

a3ed5c30bb18894f89851734b724d45e63461dcd
Romero Medeiros <romeromedeiros5@gmail.com>
2018-03-05 20:51:18 -0300

    Postgres configs

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304040517) do
+ActiveRecord::Schema.define(version: 20180304224918) do
 
-  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
     t.string "street", null: false
     t.integer "number", null: false
     t.string "cep"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20180304040517) do
     t.index ["state_id"], name: "index_addresses_on_state_id"
   end
 
-  create_table "cities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "cities", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 20180304040517) do
     t.index ["state_id"], name: "index_cities_on_state_id"
   end
 
-  create_table "neighborhoods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "neighborhoods", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,28 +45,28 @@ ActiveRecord::Schema.define(version: 20180304040517) do
     t.index ["city_id"], name: "index_neighborhoods_on_city_id"
   end
 
-  create_table "organizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "organizations", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "roles", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "states", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "states", force: :cascade do |t|
     t.string "name", null: false
     t.string "initials", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_seeds", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "user_seeds", force: :cascade do |t|
     t.string "name"
     t.string "registration"
     t.string "organization"
@@ -72,7 +75,7 @@ ActiveRecord::Schema.define(version: 20180304040517) do
     t.string "simple_address"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string "registration"
     t.string "email", default: "", null: false
     t.string "name", null: false
@@ -98,7 +101,9 @@ ActiveRecord::Schema.define(version: 20180304040517) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "auth_token"
     t.index ["address_id"], name: "index_users_on_address_id"
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
     t.index ["cpf"], name: "index_users_on_cpf"
     t.index ["email"], name: "index_users_on_email"
     t.index ["organization_id"], name: "index_users_on_organization_id"

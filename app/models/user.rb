7f1d5d9317b1894c36c0835b7760d66a6c079f1b
Romero Medeiros <romeromedeiros5@gmail.com>
2018-04-11 21:15:43 -0300

    Disabling (temporarily) address relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :address
+  # belongs_to :address
 
   validates :name, presence: true
   validates :cpf, presence: true, length: { is: 11 }

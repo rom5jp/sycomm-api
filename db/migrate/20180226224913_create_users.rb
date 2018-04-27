24d39a45c4b9136f9b927b0b872689ee7e35db5a
Romero Medeiros <romeromedeiros5@gmail.com>
2018-04-26 21:27:57 -0300

    Permitindo que alguns campos sejam nulos

--- db/migrate/20180226224913_create_users.rb
+++ db/migrate/20180226224913_create_users.rb
@@ -2,13 +2,13 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :registration, unique: true
-      t.string :email, unique: true, null: false, default: ""
-      t.string :name, null: false
+      t.string :email, unique: true
+      t.string :name
       t.string :surname
       t.string :nickname
-      t.string :cpf, unique: true, null: false
+      t.string :cpf, unique: true
       t.string :landline
-      t.string :cellphone, null: false, default: ""
+      t.string :cellphone
       t.string :whatsapp
       t.string :simple_address
       t.string :type, null: false

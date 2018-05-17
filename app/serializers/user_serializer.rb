52b381c225ff538d6d43bdd4dcfc390a6c6fe9fe
Romero Medeiros <romeromedeiros5@gmail.com>
2018-05-17 18:37:05 -0300

    User serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :auth_token, :created_at, :updated_at
+  attributes :id, :name, :email, :registration, :cpf, :auth_token, :created_at, :updated_at, :type
 end

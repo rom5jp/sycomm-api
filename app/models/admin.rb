a919b64e7cfb245efdd5c0e1a82a9976bd51c6ab
Romero Medeiros <romeromedeiros5@gmail.com>
2018-05-25 22:40:25 -0300

    Removendo validação confirmação de e-mail

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,3 @@
 class Admin < User
-  validates :email, presence: true, confirmation: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: true
 end

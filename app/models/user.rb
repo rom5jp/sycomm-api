fade5a0f707d0f6394d70e6e40d9440831ba9502
Romero Medeiros <romeromedeiros5@gmail.com>
2018-05-25 22:42:01 -0300

    Ajustes importantes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules.
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-
+  
   include DeviseTokenAuth::Concerns::User
-
-  self.inheritance_column = :type
 
   # belongs_to :address
   attr_accessor :skip_password_validation
 
   validates :name, presence: true
   validates :type, presence: true
-  validates :email, format: { with: /@/ }, uniqueness: true
   validates :auth_token, uniqueness: true
 
   scope :admins, -> { where(type: 'Admin') }
@@ -19,17 +15,18 @@ class User < ApplicationRecord
   scope :customers, -> { where(type: 'Customer') }
 
   # CALLBACKS
-  # before_save :generate_uuid!
+  before_save :generate_uuid!
   before_save :downcase_email
   before_validation :generate_auth_token!
-
-  def self.types
-    %w(Admin Employee Customer)
-  end
 
   def password_required?
     return false if skip_password_validation
     super
+  end
+
+  # sobrescrita do metodo do devise para poder retornar os valores que quero apos fazer login
+  def token_validation_response
+    { id: id, name: name, email: email, cpf: cpf, auth_token: auth_token, created_at: created_at, updated_at: updated_at, type: type }
   end
 
   private
@@ -45,4 +42,6 @@ class User < ApplicationRecord
   def downcase_email
     self.email = self.email.delete(' ').downcase
   end
+
+  
 end

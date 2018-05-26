9a23bf2f26ce37d158477639204a536cd3e57668
Romero Medeiros <romeromedeiros5@gmail.com>
2018-05-25 22:42:15 -0300

    Unificação dos controllers de User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,67 +1,82 @@
 class Api::V1::UsersController < Api::V1::BaseApiController
-  respond_to :json
+  # respond_to :json
 
   def index
-    cu = current_user
-    puts " >>>>>>>>>>>>>>> current_user: #{current_user.name}"
-    puts " >>>>>>>>>>>>>>> user_logged_in? #{user_logged_in?}"
-    render json: cu, status: 200
   end
 
   def list_paginated
     page_number = params[:page_number] || 1
     per_page = params[:per_page] || 10
 
-    users = User.joins('INNER JOIN roles ON roles.id = users.role_id')
-                .joins('INNER JOIN organizations ON organizations.id = users.organization_id')
-                .select(
-                  'users.id',
-                  'users.name',
-                  'users.type as user_type',
-                  'users.email',
-                  'users.registration',
-                  'users.cpf',
-                  'users.landline',
-                  'users.cellphone',
-                  'users.whatsapp',
-                  'users.simple_address',
-                  'roles.name as role',
-                  'organizations.name as organization'
-                )
-                .order(id: :asc)
-                .page(page_number)
-                .per(per_page)
+    user_type = params['user_type']
+
+    case user_type
+    when 'Admin'
+      users = User.admins
+                  .select(
+                    'users.id',
+                    'users.name',
+                    'users.email',
+                    'users.landline',
+                    'users.cellphone',
+                    'users.whatsapp',
+                    'users.simple_address',
+                    'users.type'
+                  )
+                  .order(id: :asc)
+                  .page(page_number)
+                  .per(per_page)
+    when 'Employee'
+      users = User.employees
+                  .select(
+                    'users.id',
+                    'users.name',
+                    'users.email',
+                    'users.cpf',
+                    'users.landline',
+                    'users.cellphone',
+                    'users.whatsapp',
+                    'users.simple_address',
+                    'users.type'
+                  )
+                  .order(id: :asc)
+                  .page(page_number)
+                  .per(per_page)
+    when 'Customer'
+      users = User.customers
+                  .joins('INNER JOIN roles ON roles.id = users.role_id')
+                  .joins('INNER JOIN organizations ON organizations.id = users.organization_id')
+                  .select(
+                    'users.id',
+                    'users.name',
+                    'users.email',
+                    'users.registration',
+                    'users.cpf',
+                    'users.landline',
+                    'users.cellphone',
+                    'users.whatsapp',
+                    'users.simple_address',
+                    'roles.name as role',
+                    'organizations.name as organization'
+                  )
+                  .order(id: :asc)
+                  .page(page_number)
+                  .per(per_page)
+    end
 
     response_data = {
       data: users,
-      total_count: User.count
+      total_count: User.where(type: user_type).count
     }
     render json: response_data, status: 200
   end
 
   def show
-    begin
-      user = User.where(id: params[:id])
-              .joins('INNER JOIN roles ON roles.id = users.role_id')
-              .joins('INNER JOIN organizations ON organizations.id = users.organization_id')
-              .select(
-                'users.id', 
-                'users.name',
-                'users.email',
-                'users.registration',
-                'users.cpf',
-                'users.landline',
-                'users.cellphone',
-                'users.whatsapp',
-                'users.simple_address',
-                'organizations.id as organization_id',
-                'organizations.name as organization',
-                'roles.id as role_id',
-                'roles.name as role',
-                'users.type as user_type'
-              ).first
+    user = User.find(params[:id])
+
+    if user.present?
       render json: user, status: 200
-    rescue
+    else
       head 404
     end
   end
@@ -83,9 +98,11 @@ class Api::V1::UsersController < Api::V1::BaseApiController
   def update
     user = User.find(user_params[:id])
 
-    if user.update_attributes(user_params)
-        render json: user, status: 200
-    else
+    begin
+      puts " >>>>> #{user.type}"
+      user.update!(user_params)
+      render json: user, status: 200
+    rescue Exception => msg
       render json: { errors: user.errors }, status: 422
     end
   end
@@ -110,7 +127,7 @@ class Api::V1::UsersController < Api::V1::BaseApiController
       :simple_address,
       :organization_id,
       :role_id,
-      :type
+      :user_type
     )
   end
 end

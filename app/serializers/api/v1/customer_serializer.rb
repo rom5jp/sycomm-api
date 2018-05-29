07cd0ad15f008c2e579e67aadc2ea55c06e673d1
Romero Medeiros <romeromedeiros5@gmail.com>
2018-05-28 23:06:21 -0300

    Pequeno ajuste nos serializers...

--- app/serializers/api/v1/customer_serializer.rb
+++ app/serializers/api/v1/customer_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :registration, :cpf, :landline, :cellphone, :whatsapp, :role_id, :organization_id, :auth_token, :created_at, :updated_at, :type
+  attributes :id, :name, :email, :registration, :cpf, :landline, :cellphone, :whatsapp, :simple_address, :role, :role_id, :organization, :organization_id, :auth_token, :created_at, :updated_at, :type
 
   belongs_to :organization
   belongs_to :role

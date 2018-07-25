8e1d8b130355bdfb68faffb1053b5b2aceb1fc2f
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-25 17:43:13 -0300

    Adicionando attrs e metodos ao Agenda serializer

--- app/serializers/agenda_serializer.rb
+++ app/serializers/agenda_serializer.rb
@@ -1,5 +1,14 @@
 class AgendaSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_date, :created_at, :updated_at
+  attributes :id, :name, :start_date, :employee_id, :customers_cpf, :created_at, :updated_at
 
-  belongs_to :user
+  # belongs_to :employee
+  # has_many :customers
+
+  def customers_cpf
+    object.customers.map { |c| c.cpf }
+  end
+
+  def start_date
+    object.start_date.strftime '%Y-%m-%d'
+  end
 end

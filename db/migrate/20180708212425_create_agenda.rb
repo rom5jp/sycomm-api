9bac4622979c5275a9e5894faea675c8ff2a4bac
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-28 16:27:54 -0300

    Removendo valor default de start_date

--- db/migrate/20180708212425_create_agenda.rb
+++ db/migrate/20180708212425_create_agenda.rb
@@ -2,7 +2,7 @@ class CreateAgenda < ActiveRecord::Migration[5.1]
   def change
     create_table :agendas do |t|
       t.string :name
-      t.date :start_date, null: false, default: Date.current
+      t.date :start_date, null: false
       t.timestamps
 
       t.belongs_to :employee, index: true
--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20180717004900) do
 
   create_table "agendas", force: :cascade do |t|
     t.string "name"
-    t.datetime "start_date", default: "2018-07-25 02:20:40", null: false
+    t.date "start_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "employee_id"

65da5427bda9b8715fadc5518b18078375c7bb9f
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-28 16:24:01 -0300

    Removendo validacao problematica (agendas: uniqueness)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,9 @@ class Customer < User
   has_and_belongs_to_many :agendas, join_table: 'agendas_customers'
 
   validates :cpf, presence: true, length: { is: 11 }
-  validates :registration, presence: true, numericality: true
-  validates :landline, allow_blank: true, length: { minimum: 8 }, numericality: true
-  validates :cellphone, presence: true, length: { is: 11 }, numericality: true
-  validates :whatsapp, allow_blank: true, length: { minimum: 8 }, numericality: true
-  validates :agendas, uniqueness: true
+  validates :registration, presence: true
+  validates :landline, allow_blank: true, length: { minimum: 8 }
+  validates :cellphone, presence: true, length: { is: 11 }
+  validates :whatsapp, allow_blank: true, length: { minimum: 8 }
+  # validates :agendas, uniqueness: true
 end

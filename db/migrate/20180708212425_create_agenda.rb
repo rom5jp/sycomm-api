edefd8ef4001dd5fa6fdd443061e932ae07b422b
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-28 16:25:18 -0300

    Usando Date em vez de Time

--- db/migrate/20180708212425_create_agenda.rb
+++ db/migrate/20180708212425_create_agenda.rb
@@ -2,7 +2,7 @@ class CreateAgenda < ActiveRecord::Migration[5.1]
   def change
     create_table :agendas do |t|
       t.string :name
-      t.datetime :start_date, null: false, default: Time.now
+      t.date :start_date, null: false, default: Date.current
       t.timestamps
 
       t.belongs_to :employee, index: true

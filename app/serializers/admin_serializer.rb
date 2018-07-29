5d8f5bc4febf74090e62d1db3fe1f00f1ceafe50
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-29 09:29:25 -0300

    Adicionando simple_address ao Admin serializer

--- app/serializers/admin_serializer.rb
+++ app/serializers/admin_serializer.rb
@@ -1,3 +1,14 @@
 class AdminSerializer < ActiveModel::Serializer
-  attributes :id, :name, :surname, :email, :cpf, :cellphone, :landline, :whatsapp, :created_at, :updated_at, :type
+  attributes :id,
+             :name,
+             :surname,
+             :email,
+             :cpf,
+             :cellphone,
+             :landline,
+             :whatsapp,
+             :simple_address,
+             :created_at,
+             :updated_at,
+             :type
 end

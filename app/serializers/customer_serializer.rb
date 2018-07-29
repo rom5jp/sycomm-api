e7cc6ea5839ff575d032b6ca3b22516ebd09c207
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-29 09:29:56 -0300

    Identacao

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,18 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes  :id, :name, :email, :registration, :cpf, :landline, :cellphone, :whatsapp, :simple_address, :public_office_id, :public_agency_id, :created_at, :updated_at, :type
+  attributes  :id,
+              :name,
+              :email,
+              :registration,
+              :cpf,
+              :landline,
+              :cellphone,
+              :whatsapp,
+              :simple_address,
+              :public_office_id,
+              :public_agency_id,
+              :created_at,
+              :updated_at,
+              :type
 
   # belongs_to :public_agency
   # belongs_to :public_office

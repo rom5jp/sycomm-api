b5891602d3e60fdc6c61c960d1cf388708f1f62d
Romero Medeiros <romeromedeiros5@gmail.com>
2018-08-02 16:32:52 -0300

    Adicionando :last_sign_in_at ao user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :cpf, :created_at, :updated_at, :type
+  attributes :id, :name, :email, :cpf, :created_at, :updated_at, :type, :last_sign_in_at
 end

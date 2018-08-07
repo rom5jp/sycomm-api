811722d7ffa54662e01bb9010622b62b2e58e755
Romero Medeiros <romeromedeiros5@gmail.com>
2018-08-06 23:59:59 -0300

    Remover atividades junto com agenda

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,6 +1,6 @@
 class Agenda < ApplicationRecord
   belongs_to :employee, class_name: 'Employee', foreign_key: 'employee_id'
-  has_many :activities
+  has_many :activities, dependent: :destroy
   has_and_belongs_to_many :customers, join_table: 'agendas_customers'
 
   validates :name, presence: true

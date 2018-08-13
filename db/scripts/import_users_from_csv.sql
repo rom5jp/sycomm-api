1d9dcad012ea616e832df81be8c53afc3be71c9e
Romero Medeiros <romeromedeiros5@gmail.com>
2018-08-13 20:29:42 -0300

    Base de clientes

--- db/scripts/import_users_from_csv.sql
+++ db/scripts/import_users_from_csv.sql
@@ -1,2 +1,2 @@
 COPY user_seeds(name, registration, public_agency, public_office, cpf, simple_address)
-FROM '/home/romero/Documentos/Sycomm/pb.csv' DELIMITER ',' CSV HEADER;
+FROM '../clientes.csv' DELIMITER ',' CSV HEADER;
